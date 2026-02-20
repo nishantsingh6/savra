001042b8cae82114b7e8f2ac44ee15d6dfc7a2c0
nishantsingh6 <nishantsingh2n@gmail.com>
2026-02-20 12:13:14 +0530

    Update backend url in frontend

--- frontend/src/services/analyticsApi.js
+++ frontend/src/services/analyticsApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000/api/analytics"
+  baseURL: "https://savra.onrender.com/api/analytics"
 });
 
 export const getSummary = () => API.get("/summary");
