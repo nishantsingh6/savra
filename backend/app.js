7bd73e0e0b15f907dcfc77c0d240e15e8094b30a
nishantsingh6 <nishantsingh2n@gmail.com>
2026-02-20 12:17:02 +0530

    Update frontend url in backend

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://code-shiksha.vercel.app"
+      "https://savra-eosin.vercel.app"
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
